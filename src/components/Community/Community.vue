35249c9d904654a00f4bbe172050f10dad4c211b
StephaneBaccam <34456841+StephaneBaccam@users.noreply.github.com>
2020-12-07 10:17:22 +0100

    Roadmap lvl 1 partie 3

--- src/components/Community/Community.vue
+++ src/components/Community/Community.vue
@@ -1,58 +1,56 @@
 <template>
-    <div class="community">
-        <div class="filter">
-            <div class="ui fluid search">
-                <div class="ui icon input">
-                    <input
-                            class="prompt"
-                            type="text"
-                            placeholder="Rechercher un utilisateur"
-                    />
-                    <i class="search icon"></i>
-                </div>
-                <div class="results"></div>
-            </div>
+  <div class="community">
+    <div class="filter">
+      <div class="ui fluid search">
+        <div class="ui icon input">
+          <input
+            class="prompt"
+            type="text"
+            placeholder="Rechercher un utilisateur"
+          />
+          <i class="search icon"></i>
         </div>
-        <div>
-
-            <div class="users">
-                <div v-for="(user, index) in users" :key="index">
-                    <div v-bind:class="[selected ? 'selected user' : 'user']" @click="toggleSelected()">
-                        <img :src="user.picture_url"/><span
-                            class=""
-                    >{{user.username}}</span
-                    >
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div class="results"></div>
+      </div>
     </div>
+    <div>
+      <div v-for="(user, index) in users" :key="index" @click="toggleSelected(user)">
+        <div :class="[user.awake ? 'selected user' : 'user']" >
+          <img :src="user.picture_url" /><span class="">{{user.username}}</span>
+        </div>
+      </div>
+    </div>
+    </div>
+    
 </template>
 
 <script>
-    import {mapGetters, mapActions} from "vuex";
+import { mapGetters, mapActions } from "vuex";
 
-    export default {
-        name: "Community",
-        data() {
-            return {
-                selected: false
-            };
-        },
-        methods: {
-            ...mapActions(["createOneToOneConversation"]),
-            openConversation() {
-                let promise = this.createOneToOneConversation("Alice");
+export default {
+  name: "Community",
+  data() {
+    return {};
+  },
+  methods: {
+    ...mapActions(["createOneToOneConversation"]),
+    openConversation() {
+      let promise = this.createOneToOneConversation("Alice");
 
-                promise.finally(() => {
-                    console.log("Conversation ouverte !");
-                });
-            }
-        },
-        computed: {
-            ...mapGetters(["users"])
-        }
-    };
+      promise.finally(() => {
+        console.log("Conversation ouverte !");
+      });
+    },
+    toggleSelected(user) {
+      console.log("Toggle Selected");
+      console.log(user);
+      user.awake = !user.awake;
+    }
+  },
+  computed: {
+    ...mapGetters(["users"])
+  }
+};
 </script>
 
 <style src="./Community.css" scoped/>
